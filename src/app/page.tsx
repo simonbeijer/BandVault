3d3fec5dd5410320780270a98e52d5ae810a8e49
Simon Beijer <beijer.simon@gmail.com>
2025-09-28 18:37:36 +0200

    Add icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,18 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen bg-background">
       <main className="flex flex-col gap-8 text-center">
         <div className="space-y-6">
+          <div className="flex flex-col items-center">
+
+          <Image
+            src="/band-vault-icon.png"
+            alt="image of a vault filled with music"
+            width={200}
+            height={200}
+            />
           <h1 className="text-4xl font-bold text-foreground">
             Welcome to Band Vault
           </h1>
-          <Image
-  src="/band-vault-icon.png"
-  alt="image of a vault filled with music"
-  width={64}
-  height={64}
-  unoptimized
-/>
+            </div>
 
           <p className="text-lg text-grey max-w-md">
             BandVault help you and you band to save and organize demo recordings. Each song gets its own space with notes, lyrics, and chat, making it easy to track ideas from practice to finished songs.
