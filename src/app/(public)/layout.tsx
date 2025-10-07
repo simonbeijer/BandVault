90d5209a429e4d9b0cea2e331b82585b2eb1b695
Simon Beijer <beijer.simon@gmail.com>
2025-10-07 15:00:48 +0200

    Remove old test header

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,11 +9,6 @@ export default function PublicLayout({ children }: PublicLayoutProps) {
   return (
     <UserProvider>
       <header className="relative flex justify-between items-center h-12 px-4">
-        <div />
-        <nav className="absolute left-1/2 transform -translate-x-1/2">
-          My navbar logged out
-        </nav>
-        <div />
       </header>
       <main>{children}</main>
     </UserProvider>
